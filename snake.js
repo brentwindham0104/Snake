01b11e30667009052e981b9a5977ffb47c7fa8cb
Brent Windham <windham_brent@yahoo.com>
2020-10-01 17:56:25 -0500

    Fixed previous bugs and added history/tail functionality

--- snake.js
+++ snake.js
@@ -4,9 +4,14 @@ class Snake {
     constructor() {
         this.body = [];
         this.body[0] = createVector(0, 0);
+        this.body[1] = createVector(120,120);
         this.body[0].x = 0
         this.body[0].y = 0;
-        this.head;
+        this.body[1].x = 0
+        this.body[1].y = 0;
+        this.head = createVector(0,0);
+        this.head.x = 0;
+        this.head.y = 0;
         background('red');
         this.food = createVector(200,200);
         this.xDirection = 0;
@@ -14,31 +19,66 @@ class Snake {
 
     }
 
+    //This should just be for updating locations, not displaying anything
     update() {
-        
-        for (var i = 0; i < this.body.length; i++) {
-            if (this.body[i].x + (this.xDirection) > 360) {
-                this.body[i].x = 0;
-            } else if (this.body[i].x + (this.xDirection) < 0) {
-                this.body[0].x = 360;
-            }  else if (this.body[i].y + (this.yDirection) > 360) {
-                this.body[i].y = 0;
-            }  else if (this.body[i].y + (this.yDirection) < 0) {
-                this.body[i].y = 360;
-            }
-            else {
-                this.head = this.body[i].x + this.xDirection;
-                this.head = this.body[i].y + this.yDirection;
-            }
-            this.body[i + 1] = this.body[i];
+        //We need to find new head values
+        //Also, we need to move down the history
+        //Then set body[0] to new head
+        // setNewHead();
+
+        //initialize head
+        //transfer history
+        //set beginning of array
+
+        if (this.body[0].x + (this.xDirection) > 360) {
+            this.head.x = 0;
+            this.head.y = this.body[0].y;
+
+        } else if (this.body[0].x + (this.xDirection) < 0) {
+            this.head.x = 360;
+            this.head.y = this.body[0].y;
+
+        }  else if (this.body[0].y + (this.yDirection) > 360) {
+            this.head.x = this.body[0].x;
+            this.head.y = 0;
+
+        }  else if (this.body[0].y + (this.yDirection) < 0) {
+            this.head.y = 360;
+            this.head.x = this.body[0].x;
+        } else {
+            this.head.x = this.body[0].x + this.xDirection;
+            this.head.y = this.body[0].y + this.yDirection;
+
         }
-        this.body[0] = this.head;   
+
+        //Head is done
+        //Now transfer history
+
+        for (var i = 0; i < this.body.length - 1; i++) {
+            console.log("Transferring " + this.body.length);
+  
+            this.body[i + 1].x = this.body[i].x;
+            this.body[i + 1].y = this.body[i].y;
+        }
+        this.body[0].x = this.head.x;
+        this.body[0].y = this.head.y;   
     }
+
     show() {
         background('red');
         let c = color(255, 204, 0);
         fill(c);
+        console.log("showing " + this.body.length);
+
         for (var i = 0; i < this.body.length; i++) {
+            console.log("body[0].x: " + this.body[0].x);
+            console.log("body[0].y: " + this.body[0].y);
+            console.log("body[1].x: " + this.body[1].x);
+            console.log("body[1].y: " + this.body[1].y);
+
+
+            //ellipse(this.body[i].x, this.body[i].y, 40, 40);
+
             rect(this.body[i].x, this.body[i].y, 40, 40);
         }
         this.addFood();
@@ -51,7 +91,7 @@ class Snake {
     nextFood() {
         this.food.x = 40 * floor(random(0,10));
         this.food.y = 40 * floor(random(0,10));
-        this.head = createVector(this.body[0].x + this.xDirection, this.body[0].y + this.yDirection);
+        //this.head = createVector(this.body[0].x + this.xDirection, this.body[0].y + this.yDirection);
     }
     addFood() {
         let c = color('blue');
