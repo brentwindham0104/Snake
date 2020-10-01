1eb86865cd6dbfe5b76d1182da10353068770024
Brent Windham <windham_brent@yahoo.com>
2020-10-01 15:46:26 -0500

    Cleaned up code but caused a bug

--- sketch.js
+++ sketch.js
@@ -1,7 +1,5 @@
-//const { DOWN_ARROW, RIGHT_ARROW, LEFT_ARROW } = require("./libraries/p5");
 
 let snake;
-// let rez = 20;
 
 function setup() {
    createCanvas(400, 400);
@@ -13,17 +11,14 @@ function setup() {
   }
   
   function draw() {
-    // scale(rez);
     if (snake.isEating()) {
-      snake.moreFood();
+      snake.nextFood();
       console.log("More food please");
     }
     snake.update();
     snake.show();
     console.log("test draw");
-
   }
-
 
   function keyPressed() {
     if (keyCode === DOWN_ARROW) {
--- snake.js
+++ snake.js
@@ -6,6 +6,7 @@ class Snake {
         this.body[0] = createVector(0, 0);
         this.body[0].x = 0
         this.body[0].y = 0;
+        this.head;
         background('red');
         this.food = createVector(200,200);
         this.xDirection = 0;
@@ -13,54 +14,45 @@ class Snake {
 
     }
 
-    // getX(){
-    //     return this.body[0].x;
-    // }
-
-    // getY(){
-    //     return this.body[0].y;
-    // }
-
-
-    update() {   
-        if (this.body[0].x + (this.xDirection) > 360) {
-            this.body[0].x = 0;
-        } else if (this.body[0].x + (this.xDirection) < 0) {
-            this.body[0].x = 360;
-        }  else if (this.body[0].y + (this.yDirection) > 360) {
-            this.body[0].y = 0;
-        }  else if (this.body[0].y + (this.yDirection) < 0) {
-            this.body[0].y = 360;
+    update() {
+        
+        for (var i = 0; i < this.body.length; i++) {
+            if (this.body[i].x + (this.xDirection) > 360) {
+                this.body[i].x = 0;
+            } else if (this.body[i].x + (this.xDirection) < 0) {
+                this.body[0].x = 360;
+            }  else if (this.body[i].y + (this.yDirection) > 360) {
+                this.body[i].y = 0;
+            }  else if (this.body[i].y + (this.yDirection) < 0) {
+                this.body[i].y = 360;
+            }
+            else {
+                this.head = this.body[i].x + this.xDirection;
+                this.head = this.body[i].y + this.yDirection;
+            }
+            this.body[i + 1] = this.body[i];
         }
-        else {
-            this.body[0].x += this.xDirection;
-            this.body[0].y += this.yDirection; 
-        } 
+        this.body[0] = this.head;   
     }
-
-
     show() {
-        // fill(0);
-        //clear();
         background('red');
         let c = color(255, 204, 0);
         fill(c);
-        rect(this.body[0].x, this.body[0].y, 40, 40);
+        for (var i = 0; i < this.body.length; i++) {
+            rect(this.body[i].x, this.body[i].y, 40, 40);
+        }
         this.addFood();
-
-        //background('red');
     }
 
     isEating() {
         return this.food.x === (this.body[0].x + this.xDirection)  && this.food.y === (this.body[0].y + this.yDirection);
     }
 
-    moreFood() {
+    nextFood() {
         this.food.x = 40 * floor(random(0,10));
         this.food.y = 40 * floor(random(0,10));
+        this.head = createVector(this.body[0].x + this.xDirection, this.body[0].y + this.yDirection);
     }
-
-
     addFood() {
         let c = color('blue');
         fill(c);
